1e34a0a057b796b475f0fb3c94e0627103124eb8
JoseOrtegaR <100377407+JoseOrtegaR@users.noreply.github.com>
2023-11-06 14:10:59 -0800

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -28,10 +28,10 @@ def get_imgs_b64(imagen_ref_base64, imagenes_base64):
     #imagenes_referencia = [cv2.imdecode(np.frombuffer(base64.b64decode(imagen_base64), np.uint8), cv2.IMREAD_GRAYSCALE) for imagen_base64 in imagenes_base64_data]
     #imagenes_referencia = [decode_base64_image(item['image_base64']) for item in imagenes_base64]
     
-    img_encoded = base64.b64decode(imagen_ref_base64)
-    img = cv2.imdecode(np.frombuffer(img_encoded, np.uint8), cv2.IMREAD_COLOR)
+    #img_encoded = base64.b64decode(imagen_ref_base64)
+    #img = cv2.imdecode(np.frombuffer(img_encoded, np.uint8), cv2.IMREAD_COLOR)
 
-    similitud = calcular_similitud(img, img)
+    #similitud = calcular_similitud(img, img)
     
     
     # Calcula la similitud con cada imagen de referencia y encuentra la máxima
@@ -42,7 +42,7 @@ def get_imgs_b64(imagen_ref_base64, imagenes_base64):
     #    return imagenes_base64_fila1[indice_max_similitud]
     #else:
     #    return None
-    return 1  #imagenes_referencia
+    return imagen_ref_base64  #imagenes_referencia
 
 # Función para calcular la similitud entre dos imágenes
 def calcular_similitud(img1, img2):
