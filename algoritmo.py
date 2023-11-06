bb4f2d261f482b46d0d71d301c3b9d346d12f88d
JoseOrtegaR <100377407+JoseOrtegaR@users.noreply.github.com>
2023-11-06 13:59:16 -0800

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -42,7 +42,7 @@ def get_imgs_b64(imagen_ref_base64, imagenes_base64):
     #    return imagenes_base64_fila1[indice_max_similitud]
     #else:
     #    return None
-    return similitud#imagenes_referencia
+    return "a"#imagenes_referencia
 
 # Función para calcular la similitud entre dos imágenes
 def calcular_similitud(img1, img2):
