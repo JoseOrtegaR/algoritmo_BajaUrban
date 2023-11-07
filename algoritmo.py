166f8bff56370e6672f44ae5a8e470796b9ad8a3
JoseOrtegaR <100377407+JoseOrtegaR@users.noreply.github.com>
2023-11-06 16:16:01 -0800

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -23,17 +23,6 @@ def get_imgs_b64(imagen_cap_base64, imagenes_ref_base64_json):
 
     imagenes_ref_base64_decod = [cv2.imdecode(np.frombuffer(base64.b64decode(imagen_base64), np.uint8), cv2.IMREAD_GRAYSCALE) for imagen_base64 in imagenes_ref_base64_data]
     
-    #imagenes_base64_data = [item['image_base64'] for item in imagenes_base64]
-
-    #imagenes_referencia = [cv2.imdecode(np.frombuffer(base64.b64decode(imagen_base64), np.uint8), cv2.IMREAD_GRAYSCALE) for imagen_base64 in imagenes_base64_data]
-    #imagenes_referencia = [decode_base64_image(item['image_base64']) for item in imagenes_base64]
-    
-    #img_encoded = base64.b64decode(imagen_ref_base64)
-    #img = cv2.imdecode(np.frombuffer(img_encoded, np.uint8), cv2.IMREAD_COLOR)
-
-    #similitud = calcular_similitud(img, img)
-    
-    
     #Calcula la similitud con cada imagen de referencia y encuentra la máxima
     similitudes = [calcular_similitud(imagen_ref, img_cap_base64_decod) for imagen_ref in imagenes_ref_base64_decod]
     max_similitud = max(similitudes)
